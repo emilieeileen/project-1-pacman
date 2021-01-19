042675aaf82d32013df93cbdc0e178b0e3b8f81d
eekulesa <emilieeileen@icloud.com>
2021-01-19 14:24:26 +0000

    Minium Viable Product :)

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const grid = document.querySelector('.grid')
 const width = 18
 const cells = []
 const holdingArea = [115, 116, 117, 118, 133, 136, 150, 151, 154, 155, 169, 170, 171, 172]
-const ghostCells = ['ghost1', 'ghost2', 'ghost3', 'ghost4']
+
 let winston = 243
 let ghost1 = 115
 let ghost2 = 118
@@ -18,7 +18,8 @@ const dogball = document.querySelector('.dogball')
 const ghostDirection = [+1, -1, +width, -width]
 const walls = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 26, 27, 35, 36, 38, 39, 40, 41, 42, 44, 45, 47, 48, 49, 50, 51, 53, 54, 56, 57, 58, 59, 60, 62, 63, 65, 66, 67, 68, 69, 71, 72, 89, 90, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 107, 108, 114, 119, 125, 126, 127, 128, 129, 130, 132, 134, 135, 137, 139, 140, 141, 142, 143, 152, 153, 162, 163, 164, 165, 166, 168, 173, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 186, 187, 188, 189, 190, 191, 193, 194, 195, 196, 197, 198, 206, 207, 215, 216, 218, 219, 220, 222, 224, 225, 227, 229, 230, 231, 233, 234, 236, 237, 238, 240, 245, 247, 248, 249, 251, 252, 258, 259, 260, 261, 262, 263, 269, 270, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 287, 288, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323]
 const chicken = [19, 34, 81, 199, 214, 321, 296]
-let bones
+
+let boneCount = 117
 const scoreCount = document.querySelector('.scorecount')
 let score = 0
 const livesCount = document.querySelector('.livescount')
@@ -91,12 +92,10 @@ document.addEventListener('keydown', (event) => {
         //moveGhost4()
       }
     } else if (cells[winston].classList.contains('ghost1') || cells[winston].classList.contains('ghost2') || cells[winston].classList.contains('ghost3') || cells[winston].classList.contains('ghost4')) {
-      cells[ghost1].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
+      winstonAndGhosts()
     } else if (cells[winston].classList.contains('bones')) {
       cells[winston].classList.remove('bones')
+      boneCount--
       score += 10
       scoreCount.innerHTML = score
     } else if (cells[winston].classList.contains('chicken')) {
@@ -134,13 +133,12 @@ document.addEventListener('keydown', (event) => {
         ghost4 = 172
         //moveGhost4()
       }
+    } else if (cells[winston].classList.contains('ghost1') || cells[winston].classList.contains('ghost2') || cells[winston].classList.contains('ghost3') || cells[winston].classList.contains('ghost4')) {
+      winstonAndGhosts()
     } else if (cells[winston].classList.contains('bones')) {
       cells[winston].classList.remove('bones')
+      boneCount--
       score += 10
-      scoreCount.innerHTML = score
-    } else if (cells[winston].classList.contains('chicken')) {
-      cells[winston].classList.remove('chicken')
-      score += 25
       scoreCount.innerHTML = score
     }
     cells[winston].classList.add('winston')
@@ -173,12 +171,10 @@ document.addEventListener('keydown', (event) => {
         //moveGhost4()
       }
     } else if (cells[winston].classList.contains('ghost1') || cells[winston].classList.contains('ghost2') || cells[winston].classList.contains('ghost3') || cells[winston].classList.contains('ghost4')) {
-      cells[ghost1].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
+      winstonAndGhosts()
     } else if (cells[winston].classList.contains('bones')) {
       cells[winston].classList.remove('bones')
+      boneCount--
       score += 10
       scoreCount.innerHTML = score
     } else if (cells[winston].classList.contains('chicken')) {
@@ -216,8 +212,11 @@ document.addEventListener('keydown', (event) => {
         ghost4 = 172
         //moveGhost4()
       }
+    } else if (cells[winston].classList.contains('ghost1') || cells[winston].classList.contains('ghost2') || cells[winston].classList.contains('ghost3') || cells[winston].classList.contains('ghost4')) {
+      winstonAndGhosts() 
     } else if (cells[winston].classList.contains('bones')) {
       cells[winston].classList.remove('bones')
+      boneCount--
       score += 10
       scoreCount.innerHTML = score
       cells[winston].classList.add('winston')
@@ -251,12 +250,10 @@ document.addEventListener('keydown', (event) => {
         //moveGhost4()
       }
     } else if (cells[winston].classList.contains('ghost1') || cells[winston].classList.contains('ghost2') || cells[winston].classList.contains('ghost3') || cells[winston].classList.contains('ghost4')) {
-      cells[ghost1].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
+      winstonAndGhosts()
     } else if (cells[winston].classList.contains('bones')) {
       cells[winston].classList.remove('bones')
+      boneCount--
       score += 10
       scoreCount.innerHTML = score
     } else if (cells[winston].classList.contains('chicken')) {
@@ -296,12 +293,10 @@ document.addEventListener('keydown', (event) => {
         //moveGhost4()
       }
     } else if (cells[winston].classList.contains('ghost1') || cells[winston].classList.contains('ghost2') || cells[winston].classList.contains('ghost3') || cells[winston].classList.contains('ghost4')) {
-      cells[ghost1].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
+      winstonAndGhosts()
     } else if (cells[winston].classList.contains('bones')) {
       cells[winston].classList.remove('bones')
+      boneCount--
       score += 10
       scoreCount.innerHTML = score
     } else if (cells[winston].classList.contains('chicken')) {
@@ -312,7 +307,8 @@ document.addEventListener('keydown', (event) => {
     }
     cells[winston].classList.add('winston')
   }
-  // checkForWin()
+  checkForWin()
+  console.log(boneCount)
 })
 
 /*
@@ -325,11 +321,8 @@ function ghost1RandomMovements() {
   clearInterval(ghost1Interval)
   let direction = ghostDirection[Math.floor(Math.random() * ghostDirection.length)]
   ghost1Interval = setInterval(() => {
-    if (cells[ghost1 + direction].classList.contains('winston')) {
-      cells[ghost1 + direction].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
+    if (cells[winston].classList.contains('ghost1')) {
+      winstonAndGhosts()
     } else if (!cells[ghost1 + direction].classList.contains(ghost2) && !cells[ghost1 + direction].classList.contains('walls') && !cells[ghost1 + direction].classList.contains('holdingArea')) {
       if (cells[ghost1].classList.contains('dogball')) {
         cells[ghost1].classList.remove('ghost1', 'dogball')
@@ -348,11 +341,8 @@ function ghost2RandomMovements() {
   clearInterval(ghost2Interval)
   let direction = ghostDirection[Math.floor(Math.random() * ghostDirection.length)]
   ghost2Interval = setInterval(() => {
-    if (cells[ghost2 + direction].classList.contains('winston')) {
-      cells[ghost2 + direction].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
+    if (cells[winston].classList.contains('ghost2')) {
+      winstonAndGhosts()
     } else if (!cells[ghost2 + direction].classList.contains(ghost1) && !cells[ghost2 + direction].classList.contains('walls') && !cells[ghost2 + direction].classList.contains('holdingArea')) {
       if (cells[ghost2].classList.contains('dogball')) {
         cells[ghost2].classList.remove('ghost2', 'dogball')
@@ -363,11 +353,6 @@ function ghost2RandomMovements() {
         ghost2 += direction
         cells[ghost2].classList.add('ghost2')
       }
-    } else if (cells[ghost2].classList.contains('winston')) {
-      cells[ghost2].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
     } else direction = ghostDirection[Math.floor(Math.random() * ghostDirection.length)]
   }, 500)
 }
@@ -376,11 +361,8 @@ function ghost3RandomMovements() {
   clearInterval(ghost3Interval)
   let direction = ghostDirection[Math.floor(Math.random() * ghostDirection.length)]
   ghost3Interval = setInterval(() => {
-    if (cells[ghost3 + direction].classList.contains('winston')) {
-      cells[ghost3 + direction].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
+    if (cells[winston].classList.contains('ghost3')) {
+      winstonAndGhosts()
     } else if (!cells[ghost3 + direction].classList.contains(ghost4) && !cells[ghost3 + direction].classList.contains('walls') && !cells[ghost3 + direction].classList.contains('holdingArea')) {
       if (cells[ghost3].classList.contains('dogball')) {
         cells[ghost3].classList.remove('ghost3', 'dogball')
@@ -400,11 +382,8 @@ function ghost4RandomMovements() {
   clearInterval(ghost4Interval)
   let direction = ghostDirection[Math.floor(Math.random() * ghostDirection.length)]
   ghost4Interval = setInterval(() => {
-    if (cells[ghost4 + direction].classList.contains('winston')) {
-      cells[ghost4 + direction].classList.remove('winston')
-      lives -= 1
-      livesCount.innerHTML = lives
-      checkLives()
+    if (cells[winston].classList.contains('ghost4')) {
+      winstonAndGhosts()
     } else if (!cells[ghost4 + direction].classList.contains(ghost3) && !cells[ghost4 + direction].classList.contains('walls') && !cells[ghost4 + direction].classList.contains('holdingArea')) {
       if (cells[ghost4].classList.contains('dogball')) {
         cells[ghost4].classList.remove('ghost4', 'dogball')
@@ -485,6 +464,7 @@ function moveRight4() {
 
 // get all ghost out of holding and moving randomly on start click
 function moveGhost1() {
+  clearInterval(ghost1Interval)
   const downInterval = setInterval(() => {
     cells[ghost1].classList.remove('ghost1')
     ghost1 += 18
@@ -497,6 +477,7 @@ function moveGhost1() {
 }
 
 function moveGhost2() {
+  clearInterval(ghost2Interval)
   const downInterval2 = setInterval(() => {
     cells[ghost2].classList.remove('ghost2')
     ghost2 += 18
@@ -508,6 +489,7 @@ function moveGhost2() {
   }, 1000)
 }
 function moveGhost3() {
+  clearInterval(ghost3Interval)
   const upInterval3 = setInterval(() => {
     cells[ghost3].classList.remove('ghost3')
     ghost3 -= 18
@@ -519,6 +501,7 @@ function moveGhost3() {
   }, 2500)
 }
 function moveGhost4() {
+  clearInterval(ghost4Interval)
   const upInterval4 = setInterval(() => {
     cells[ghost4].classList.remove('ghost4')
     ghost4 -= 18
@@ -547,6 +530,36 @@ function setDogBalls() {
       cells[ghost2].classList.add('dogball')
       cells[ghost3].classList.add('dogball')
       cells[ghost4].classList.add('dogball')
+      if (cells[115].classList.contains('dogball')) {
+        cells[115].classList.remove('dogball')
+      }
+      if (cells[118].classList.contains('dogball')) {
+        cells[118].classList.remove('dogball')
+      }
+      if (cells[133].classList.contains('dogball')) {
+        cells[133].classList.remove('dogball')
+      }
+      if (cells[136].classList.contains('dogball')) {
+        cells[136].classList.remove('dogball')
+      }
+      if (cells[150].classList.contains('dogball')) {
+        cells[150].classList.remove('dogball')
+      }
+      if (cells[151].classList.contains('dogball')) {
+        cells[151].classList.remove('dogball')
+      }
+      if (cells[154].classList.contains('dogball')) {
+        cells[154].classList.remove('dogball')
+      }
+      if (cells[155].classList.contains('dogball')) {
+        cells[155].classList.remove('dogball')
+      }
+      if (cells[169].classList.contains('dogball')) {
+        cells[169].classList.remove('dogball')
+      }
+      if (cells[172].classList.contains('dogball')) {
+        cells[172].classList.remove('dogball')
+      }
     })
     setTimeout(() => {
       clearInterval(dogballTimer)
@@ -557,13 +570,16 @@ function setDogBalls() {
       if (ghost1 === 115) {
         console.log('ghost1 move')
         moveGhost1()
-      } else if (ghost2 === 118) {
+      }
+      if (ghost2 === 118) {
         console.log('ghost2 move')
         moveGhost2()
-      } else if (ghost3 === 169) {
+      }
+      if (ghost3 === 169) {
         console.log('ghost3 move')
         moveGhost3()
-      } else if (ghost4 === 172) {
+      }
+      if (ghost4 === 172) {
         console.log('ghost4 move')
         moveGhost4()
       } else
@@ -571,25 +587,58 @@ function setDogBalls() {
     }, 5000)
   }
 }
-
+// collision testing. Winston does not reappear when ghost move over a stationary winston. must then use right or left arrow to make him visible near start point.
+function winstonAndGhosts() {
+  if (cells[winston].classList.contains('ghost1')) {
+    cells[ghost1].classList.remove('winston')
+    winston = 243
+    cells[winston].classList.add('winston')
+    lives -= 1
+    livesCount.innerHTML = lives
+    checkLives()
+  } else if (cells[winston].classList.contains('ghost2')) {
+    cells[ghost2].classList.remove('winston')
+    winston = 243
+    cells[winston].classList.add('winston')
+    lives -= 1
+    livesCount.innerHTML = lives
+    checkLives()
+  } else if (cells[winston].classList.contains('ghost3')) {
+    cells[ghost3].classList.remove('winston')
+    winston = 243
+    cells[winston].classList.add('winston')
+    lives -= 1
+    livesCount.innerHTML = lives
+    checkLives()
+  } else if (cells[winston].classList.contains('ghost4')) {
+    cells[ghost4].classList.remove('winston')
+    winston = 243
+    cells[winston].classList.add('winston')
+    lives -= 1
+    livesCount.innerHTML = lives
+    checkLives()
+  }
+}
 
 // check lives
 function checkLives() {
   if (lives === 0) {
     alert(`Thanks for playing! Your score was ${score}!`)
     window.location.reload()
-  } else {
-    winston = 243
-  }
+  } else
+    return
 }
 
 function checkForWin() {
   //   // if grid does not contain any cells with class of bones
-  if (!cells[bones].classList.contains('bones')) {
-    console.log('win check')
+  if (boneCount === 0) {
     alert(`You win! Your score was ${score}!`)
     window.location.reload()
   } else {
     return
   }
+}
+
+function restartGame() {
+  window.location.reload()
 }
